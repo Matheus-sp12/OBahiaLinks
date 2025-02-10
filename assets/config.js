9ec85598374032b439ff3bf549e419d047c17b46
matheus <viniciusomagnifico@hotmail.com>
2025-02-09 22:22:45 -0300

    first commit

---
+++ assets/config.js
@@ -0,0 +1,27 @@
+let config = {
+  "Title": "O Bahia Links",
+  "Name": "O Bahia Links",
+  "Description": "Venha conhecer o primeiro Gastrobar da região",
+  "links": [
+    {
+      "Title": "Cardápio",
+      "URL": "https://o-bahia-gastrobar.vercel.app/",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Ifood",
+      "URL": "https://www.ifood.com.br/delivery/sao-paulo-sp/o-bahia-gastrobar-vila-tolstoi/7bf0c889-7f02-4d15-845e-d4adb4b79a55?UTM_Medium=share",
+      "icon_classes": "fa fa-truck",
+    },
+    {
+      "Title": "Localização",
+      "URL": "https://maps.app.goo.gl/f8utFajSiJNApZD28",
+      "icon_classes": "fa fa-map"
+    },
+    {
+      "Title": "WhatsApp",
+      "URL": "https://wa.me/5511988345332",
+      "icon_classes": "fab fa-whatsapp"
+    },
+  ]
+}
